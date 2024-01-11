581da620384e528bb46420132eecc81989896fda
NghiNguyen <nghinguyenzero@gmail.com>
2024-01-11 12:54:37 +0700

    works add

--- components/form/editor-field.tsx
+++ components/form/editor-field.tsx
@@ -1,6 +1,6 @@
 import { Box, FormHelperText, Typography } from '@mui/material'
 import dynamic from 'next/dynamic'
-import { LegacyRef,useCallback, useEffect, useRef } from 'react'
+import { LegacyRef, useCallback, useEffect, useRef } from 'react'
 import { Control, FieldValues, Path, useController } from 'react-hook-form'
 import ReactQuill, { ReactQuillProps } from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
@@ -37,40 +37,52 @@ export function EditorField<T extends FieldValues>({ name, control, label }: Edi
 	})
 
 	const editorRef = useRef(null)
-
 	const cloudinaryWidgetRef = useRef(null)
 
 	useEffect(() => {
-		// @ts-ignore no type def support yet
-		const widget = window.cloudinary.createUploadWidget(
-			{
-				cloudName: 'easy-frontend',
-				uploadPreset: 'learn-nextjs',
-				multiple: false, //restrict upload to a single file
-				clientAllowedFormats: ['image'], //restrict uploading to image files only
-				maxImageFileSize: 2000000, //restrict file size to less than 2MB
-				// sources: [ "local", "url"], // restrict the upload sources to URL and local files
-				// folder: "user_images", //upload files to the specified folder
-				// tags: ["users", "profile"], //add the given tags to the uploaded files
-				// context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-				// maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
-				// theme: "purple", //change to a purple theme
-			},
-			// @ts-ignore no type support yet
-			(error, result) => {
-				if (!error && result && result.event === 'success') {
-					const quill = editorRef.current
-					// @ts-ignore
-					const range = quill?.getEditorSelection?.()
-					if (quill && range) {
+		function initCloudinaryWidget() {
+			// check and retry if cloudinary not ready
+			// @ts-ignore
+			if (!window.cloudinary) {
+				console.log('cloudinary not ready, trigger retry in 500ms')
+				setTimeout(initCloudinaryWidget, 500)
+				return
+			}
+
+			console.log('cloudinary is ready')
+			// @ts-ignore no type def support yet
+			const widget = window.cloudinary.createUploadWidget(
+				{
+					cloudName: 'easy-frontend',
+					uploadPreset: 'learn-nextjs',
+					multiple: false, //restrict upload to a single file
+					clientAllowedFormats: ['image'], //restrict uploading to image files only
+					maxImageFileSize: 2000000, //restrict file size to less than 2MB
+					// sources: [ "local", "url"], // restrict the upload sources to URL and local files
+					// folder: "user_images", //upload files to the specified folder
+					// tags: ["users", "profile"], //add the given tags to the uploaded files
+					// context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+					// maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
+					// theme: "purple", //change to a purple theme
+				},
+				// @ts-ignore no type support yet
+				(error, result) => {
+					if (!error && result && result.event === 'success') {
+						const quill = editorRef.current
 						// @ts-ignore
-						quill.getEditor()?.insertEmbed?.(range.index, 'image', result.info?.secure_url)
+						const range = quill?.getEditorSelection?.()
+						if (quill && range) {
+							// @ts-ignore
+							quill.getEditor()?.insertEmbed?.(range.index, 'image', result.info?.secure_url)
+						}
 					}
 				}
-			}
-		)
+			)
 
-		cloudinaryWidgetRef.current = widget
+			cloudinaryWidgetRef.current = widget
+		}
+
+		initCloudinaryWidget()
 	}, [])
 
 	const imageHandler = useCallback(() => {
@@ -89,7 +101,6 @@ export function EditorField<T extends FieldValues>({ name, control, label }: Edi
 				['clean'],
 			],
 			handlers: {
-				// image: imageHandler,
 				image: imageHandler,
 			},
 		},
--- hooks/index.ts
+++ hooks/index.ts
@@ -2,5 +2,7 @@ export * from './use-auth'
 export * from './use-work-list'
 export * from './use-tag-list'
 export * from './use-work-details'
+export * from './use-work-add'
+
 
 
